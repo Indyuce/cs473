eec3d3d0bb96eafd78965e97ff9ff1413f10e23f
jules <jules.peyrat@epfl.ch>
2024-10-28 10:31:55 +0100

    removed built files

--- bouncingBall/build-release/src/bouncingBall.c.d
+++
@@ -1,3 +0,0 @@
-build-release/src/bouncingBall.c.o: src/bouncingBall.c \
- support/include/stdio.h support/include/printf.h support/include/vga.h \
- support/include/swap.h support/include/defs.h support/include/stdint.h
